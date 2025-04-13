7cf9525a39d2ef9aa1c0703fc080c84ca3c4ca10
Chris Pequignot <chris.lepek@gmail.com>
2025-04-13 19:17:36 +0200

    revue main api sans streamlit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-import streamlit as st
+from fastapi import FastAPI, HTTPException
 import joblib
 import numpy as np
 from pydantic import BaseModel
@@ -12,70 +11,51 @@ CSV_PATH = './small_data.csv'
 SCALER_PATH  = './scaler.joblib'
 MODEL_PATH = './model.joblib'
 
-# === Chargement des ressources ===
-@st.cache_data
-def load_data():
-    return pd.read_csv(CSV_PATH)
+# Charge les données
+df = pd.read_csv(CSV_PATH)
+scaler = joblib.load(SCALER_PATH)
+model = joblib.load(MODEL_PATH)
 
-@st.cache_resource
-def load_scaler():
-    return joblib.load(SCALER_PATH)
-
-@st.cache_resource
-def load_model():
-    return joblib.load(MODEL_PATH)
-
-df = load_data()
-scaler = load_scaler()
-model = load_model()
-
+# FastAPI app
+app = FastAPI()
 
 
 #Fonction  pour récupérer les données d'un client
-
 def get_client_data(client_id: int):
     client_row = df[df['SK_ID_CURR'] == client_id]
 
     if client_row.empty:
         return None  # Client ID non trouvé
-   
-    return client_row # Extraire les features
+
+    # Extraire les features
+    features = client_row
+    return features
 
 
-# ------------------------------Interface Streamlit-----------------------
-
-st.title("Prédiction de Défaut de Client")
-
-# === Entrée utilisateur ===
-client_id = st.number_input("Entrez le Client ID", min_value=0, step=1)
-
-
-# === Bouton : Prédiction ===
-
-if st.button("Prédire"):
+@app.get("/predict/{client_id}")
+def predict(client_id: int):
+    # Vérifier si le client existe
     client_features = get_client_data(client_id)
 
     if client_features is None:
+        raise HTTPException(status_code=404, detail="Client not found")
 
-        st.error(f"Client {client_id} non trouvé")
-    else:
-        # Préparer les données pour la prédiction
-            # Convertir les données en tableau 2D avec numpy
-        client_features_2d = np.array(client_features).reshape(1, -1)
+    # Préparer les données pour la prédiction
+    # Convertir les données en tableau 2D avec numpy
+    client_features_2d = np.array(client_features).reshape(1, -1)
+    scaled_features = scaler.transform(client_features_2d)
 
-        # Préparer les données pour la prédiction
-        scaled_features = scaler.transform(client_features_2d)
+    # Prédiction du modèle
+    proba = model.predict_proba(scaled_features)
+    print(proba)  # Probabilité de la classe "défaut"
+    proba = proba[:, 1]
 
-        # Prédiction du modèle
-        proba = model.predict_proba(scaled_features)
-        print(proba)  # Probabilité de la classe "défaut"
-        proba = proba[:, 1]
+    # Appliquer le seuil optimisé
+    seuil = 0.07
+    classe = "Accepté" if proba <= seuil else "Refusé"
 
-        # Appliquer le seuil optimisé (par exemple, 0.06)
-        seuil = 0.07
-        classe = "Accepté" if proba <= seuil else "Refusé"
-        
-        st.write(f"Client ID: {client_id}")
-        st.write(f"Probabilité de défaut: {proba[0]:.4f}")
-        st.write(f"Classe prédite: {classe}")
-   
+    return {"client_id": client_id, "probabilité": proba[0], "classe": classe}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
